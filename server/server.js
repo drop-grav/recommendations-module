a6c1191cab07efa90be751796f00d2f982c0ec22
Howard Yang <howardyang25@gmail.com>
2019-09-11 18:02:47 -0700

    Added CRUD routes and updated Readme

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const app = express();
 
 app.use(require('morgan')('dev'));
 
+app.use(express.json());
+
 app.use(cors());
 app.use('/listing/:id', express.static('public'));
 
@@ -15,6 +17,25 @@ app.get('/api/nearbyPlaces/:id', (req, res) => {
   const randomAmount = Math.floor(Math.random() * 6 + 8);
   model.Place.aggregate([{ $sample: { size: randomAmount } }]).then((result) => {
     res.send(result);
+  });
+});
+
+app.post('/api/nearbyPlaces', (req, res) => {
+  model.Place.create([req.body]);
+  res.status(201).send();
+});
+
+app.put('/api/nearbyPlaces', (req, res) => {
+  //FIXME
+  model.Place.update({
+    url: 'https://drop-grav-recommendations.s3-us-west-1.amazonaws.com/64.jpg',
+  }, { url: 'test.test' });
+  res.send();
+});
+
+app.delete('/api/nearbyPlaces', (req, res) => {
+  model.Place.deleteOne(req.body, () => {
+    res.send();
   });
 });
 
